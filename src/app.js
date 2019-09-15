5c9b6c6a54fa6abc414a6c31581b54842a2e21c7
Egorov Aleksey <aleksey89-lex@mail.ru>
2019-09-15 22:40:32 +0300

    added get json base

--- src/app.js
+++ src/app.js
@@ -1,148 +1,119 @@
-let health = document.getElementById('health');
-health = 100;
-let armor = document.getElementById('armor');
-armor = 10;
-
-let attack = document.getElementById('attack');
-attack = 25;
-
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/test.json');
+xhr.addEventListener('load', () => {
+    const data = JSON.parse(xhr.responseText);
+    const {person} = data;
+    const {helmets} = data.equipment;
+    const {chests} = data.equipment;
+const {boots} = data.equipment;
+const {gloves} = data.equipment;
+const {swords} = data.equipment;
 
 
 // показ sword
+    function showSword() {
+        const sword = document.querySelector('.sword');
+        const img = document.createElement('IMG');
+        img.src = swords[0].img;
+        sword.appendChild(img);
+        sword.style.display = 'none';
+        const swordAdd = document.getElementById('showWeapon5');
+        swordAdd.addEventListener('click', () => {
+            event.preventDefault();
+            sword.style.display = '';
 
-function showSword() {
-    const sword = document.querySelector('.sword');
-    const img = document.createElement('IMG');
-    img.src = 'http://test.datalb.ru/assets/Sword-2.png';
-    sword.appendChild(img);
-    sword.style.display = 'none';
-    const swordAdd = document.getElementById('showWeapon5');
-    swordAdd.addEventListener('click', () => {
-        event.preventDefault();
-        sword.style.display = '';
-        health += 0;
-        armor += 0;
-        attack += 25;
-    });
-
-}
-
+        });
+    }
 // end показ sword
 
-//показ boots
-function showBoots() {
-    const boots = document.querySelector('.boots');
-    const img = document.createElement('IMG');
-    img.src = 'http://test.datalb.ru/assets/Boots-2.png';
-    boots.appendChild(img);
-    boots.style.display = 'none';
-    const bootsAdd = document.getElementById('showWeapon4');
-    bootsAdd.addEventListener('click', () => {
-        event.preventDefault();
-        boots.style.display = '';
-        health += 10;
-        armor += 4;
-        attack += 0;
-    });
-}
-
-// end показ boots
-
-
 //показ gloves
-function showGloves() {
-    const gloves = document.querySelector('.gloves');
-    const img = document.createElement('IMG');
-    img.src = 'http://test.datalb.ru/assets/Gloves-2.png';
-    gloves.appendChild(img);
-    gloves.style.display = 'none';
-    const glovesAdd = document.getElementById('showWeapon3');
-    glovesAdd.addEventListener('click', () => {
-        event.preventDefault();
-        gloves.style.display = '';
-        health += 10;
-        armor += 4;
-        attack += 0;
-    });
-}
+    function showGloves() {
+        const glove = document.querySelector('.gloves');
+        const img = document.createElement('IMG');
+        img.src = gloves[0].img;
+        glove.appendChild(img);
+        glove.style.display = 'none';
+        const glovesAdd = document.getElementById('showWeapon3');
+        glovesAdd.addEventListener('click', () => {
+            event.preventDefault();
+            glove.style.display = '';
 
+        });
+    }
 // end показ gloves
 
-// показ chest
-function showChest() {
-    const chest = document.querySelector('.chest');
-    const img = document.createElement('IMG');
-    img.src = 'http://test.datalb.ru/assets/Chest-2.png';
-    chest.appendChild(img);
-    chest.style.display = 'none';
-    const chestAdd = document.getElementById('showWeapon2');
-    chestAdd.addEventListener('click', () => {
-        event.preventDefault();
-        chest.style.display = '';
-        health += 30;
-        armor += 15;
-        attack += 0;
-    });
-}
+//показ boots
+    function showBoots() {
+        const boot = document.querySelector('.boots');
+        const img = document.createElement('IMG');
+        img.src = boots[0].img;
+        boot.appendChild(img);
+        boot.style.display = 'none';
+        const bootsAdd = document.getElementById('showWeapon4');
+        bootsAdd.addEventListener('click', () => {
+            event.preventDefault();
+            boot.style.display = '';
 
+        });
+    }
+// end показ boots
+
+// показ chest
+        function showChest() {
+        const chest = document.querySelector('.chest');
+        const img = document.createElement('IMG');
+        img.src = chests[0].img;
+        chest.appendChild(img);
+        chest.style.display = 'none';
+        const chestAdd = document.getElementById('showWeapon2');
+        chestAdd.addEventListener('click', () => {
+            event.preventDefault();
+            chest.style.display = '';
+
+        });
+    }
 // end показ chest
 
-// показ Helmet
-function showHelmet() {
-    const equipment = document.querySelector('.helmet');
-    const img = document.createElement('IMG');
-    img.src = 'http://test.datalb.ru/assets/Helmet-2.png';
-    equipment.appendChild(img);
-    equipment.style.display = 'none';
-    const helmet = document.getElementById('showWeapon1');
-    helmet.addEventListener('click', () => {
-        event.preventDefault();
-        equipment.style.display = '';
-        health += 10;
-        armor += 4;
-        attack += 0;
-    });
-}
+    // показ Helmet
+    function showHelmet() {
+        const equipment = document.querySelector('.helmet');
+        const img = document.createElement('IMG');
+        img.src = helmets[0].img;
+      //  console.log(helmets[0].img);
+        equipment.appendChild(img);
+        equipment.style.display = 'none';
+        const helmet = document.getElementById('showWeapon1');
+        helmet.addEventListener('click', () => {
+            event.preventDefault();
+            equipment.style.display = '';
 
-//end Helmet
-
-
-function getHeroImg() {
-    const hero = document.querySelector('.hero-img');
-    const img = document.createElement('IMG');
-    img.src = 'http://test.datalb.ru/assets/Dude-2.png';
-    hero.appendChild(img);
-}
-
-
-function getData() {
-    const goodsData = document.getElementById('maket');
-    return fetch('test.json')
-        .then((responce) => {
-
-            if (responce.ok) {
-                return responce.json();
-            } else {
-                throw new Error('Данные не были получены, ошибка: ' + responce.status);
-            }
-        })
-        .then((data) => {
-            return data;
-        })
-
-
-        .catch((err) => {
-            console.warn(err);
-            goodsData.innerHTML = '<div style="color: red; font-size: 30px">Упс, что-то пошло не так!</div>'
         });
+    }
+//end Helmet
+    function getHeroImg() {
+        const hero = document.querySelector('.hero-img');
+        const img = document.createElement('IMG');
+        img.src = person.img;
+        hero.appendChild(img);
+    }
 
-}
+    getHeroImg();
+    showHelmet();
+    showChest();
+    showBoots();
+    showGloves();
+    showSword();
+});
+xhr.send();
 
 
-getData();
-showSword();
-getHeroImg();
-showBoots();
-showChest();
-showGloves();
-showHelmet();
+
+
+
+
+
+
+
+
+
+
