4360655ec1f91979d99d7423930bd9960fdfe8e0
Egorov Aleksey <aleksey89-lex@mail.ru>
2019-09-19 04:23:09 +0300

    select option is work

--- src/app.js
+++ src/app.js
@@ -5,91 +5,123 @@ xhr.addEventListener('load', () => {
     const {person} = data;
     const {helmets} = data.equipment;
     const {chests} = data.equipment;
-const {boots} = data.equipment;
-const {gloves} = data.equipment;
-const {swords} = data.equipment;
-
+    const {boots} = data.equipment;
+    const {gloves} = data.equipment;
+    const {swords} = data.equipment;
 
 // показ sword
     function showSword() {
         const sword = document.querySelector('.sword');
-        const img = document.createElement('IMG');
-        img.src = swords[0].img;
-        sword.appendChild(img);
-        sword.style.display = 'none';
         const swordAdd = document.getElementById('showWeapon5');
-        swordAdd.addEventListener('click', () => {
-            event.preventDefault();
-            sword.style.display = '';
-
+        const swordEl = document.querySelector('[data-id=swords]');
+        swords.map(o => {
+            const option = new Option(o.name);
+            swordEl.appendChild(option);
+            swordAdd.addEventListener('click', () => {
+                const img = document.createElement('IMG');
+                if (swordEl.value === o.name) {
+                    sword.innerHTML = '';
+                    img.src = o.img;
+                    console.log(img);
+                    sword.appendChild(img);
+                }
+            });
         });
     }
+
 // end показ sword
-
-//показ gloves
-    function showGloves() {
-        const glove = document.querySelector('.gloves');
-        const img = document.createElement('IMG');
-        img.src = gloves[0].img;
-        glove.appendChild(img);
-        glove.style.display = 'none';
-        const glovesAdd = document.getElementById('showWeapon3');
-        glovesAdd.addEventListener('click', () => {
-            event.preventDefault();
-            glove.style.display = '';
-
-        });
-    }
-// end показ gloves
 
 //показ boots
     function showBoots() {
         const boot = document.querySelector('.boots');
-        const img = document.createElement('IMG');
-        img.src = boots[0].img;
-        boot.appendChild(img);
-        boot.style.display = 'none';
-        const bootsAdd = document.getElementById('showWeapon4');
-        bootsAdd.addEventListener('click', () => {
-            event.preventDefault();
-            boot.style.display = '';
-
+        const bootsAdd = document.getElementById('showWeapon3');
+        const bootsEl = document.querySelector('[data-id=boots]');
+        boots.map(o => {
+            const option = new Option(o.name);
+            bootsEl.appendChild(option);
+            bootsAdd.addEventListener('click', () => {
+                const img = document.createElement('IMG');
+                if (bootsEl.value === o.name) {
+                    boot.innerHTML = '';
+                    img.src = o.img;
+                    console.log(img);
+                    boot.appendChild(img);
+                }
+            });
         });
     }
+
 // end показ boots
 
-// показ chest
-        function showChest() {
-        const chest = document.querySelector('.chest');
-        const img = document.createElement('IMG');
-        img.src = chests[0].img;
-        chest.appendChild(img);
-        chest.style.display = 'none';
-        const chestAdd = document.getElementById('showWeapon2');
-        chestAdd.addEventListener('click', () => {
-            event.preventDefault();
-            chest.style.display = '';
-
+//показ gloves
+    function showGloves() {
+        const glove = document.querySelector('.gloves');
+        const glovesAdd = document.getElementById('showWeapon4');
+        const gloveEl = document.querySelector('[data-id=gloves]');
+        gloves.map(o => {
+            const option = new Option(o.name);
+            gloveEl.appendChild(option);
+            glovesAdd.addEventListener('click', () => {
+                const img = document.createElement('IMG');
+                console.log(img);
+                if (gloveEl.value === o.name) {
+                    glove.innerHTML = '';
+                    img.src = o.img;
+                    console.log(img);
+                    glove.appendChild(img);
+                }
+            });
         });
     }
+
+// end показ gloves
+
+// показ chest
+    function showChest() {
+        const chest = document.querySelector('.chest');
+        const chestAdd = document.getElementById('showWeapon2');
+        const chestEl = document.querySelector('[data-id=chest]');
+        chests.map(o => {
+            const option = new Option(o.name);
+            chestEl.appendChild(option);
+            chestAdd.addEventListener('click', () => {
+                const img = document.createElement('IMG');
+                console.log(img);
+                if (chestEl.value === o.name) {
+                    chest.innerHTML = '';
+                    img.src = o.img;
+                    console.log(img);
+                    chest.appendChild(img);
+                }
+            });
+        });
+    }
+
 // end показ chest
 
     // показ Helmet
     function showHelmet() {
-        const equipment = document.querySelector('.helmet');
-        const img = document.createElement('IMG');
-        img.src = helmets[0].img;
-      //  console.log(helmets[0].img);
-        equipment.appendChild(img);
-        equipment.style.display = 'none';
-        const helmet = document.getElementById('showWeapon1');
-        helmet.addEventListener('click', () => {
-            event.preventDefault();
-            equipment.style.display = '';
-
+        const helmetClass = document.querySelector('.helmet');
+        const buttonHelmet = document.getElementById('showWeapon1');
+        const helmetEl = document.querySelector('[data-id=system');
+        helmets.map(o => {
+            const option = new Option(o.name);
+            helmetEl.appendChild(option);
+            buttonHelmet.addEventListener('click', () => {
+                console.log(helmets);
+                const img = document.createElement('IMG');
+                if (helmetEl.value === o.name) {
+                    helmetClass.innerHTML = '';
+                    img.src = o.img;
+                    console.log(img);
+                    helmetClass.appendChild(img);
+                }
+            })
         });
     }
+
 //end Helmet
+
     function getHeroImg() {
         const hero = document.querySelector('.hero-img');
         const img = document.createElement('IMG');
@@ -105,15 +137,3 @@ const {swords} = data.equipment;
     showSword();
 });
 xhr.send();
-
-
-
-
-
-
-
-
-
-
-
-
